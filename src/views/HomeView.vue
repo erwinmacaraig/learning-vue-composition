d1380a79318ef4e6a2acf3b168b39750a5e6b834
Erwin Macaraig <erwin.macaraig@enablesme.com.au>
2022-05-24 13:45:45 +0800

    refs demo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 
 <template>
   <div class="home">
+  <h3>{{ counterTitle}}:</h3>
     <div>
     <button @click="decreaseCounter" class="btn">-</button>
     <span class="counter">{{counter}}</span>
     <button @click="increaseCounter" class="btn">+</button>
+    </div>
+    <div class="edit">
+      <h4>Edit counter title</h4>
+      <input type="text" v-model="counterTitle">
     </div>
   </div>
 </template>
@@ -57,6 +62,11 @@ export default {
   import { ref } from  'vue'
   
   const counter = ref(0)
+  const counterTitle = ref('My Counter')
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter'
+  })
 
   const increaseCounter = () => {
     counter.value++
@@ -75,5 +85,9 @@ export default {
 .btn, .counter {
   font-size: 40px;
   margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
 }
 </style>
