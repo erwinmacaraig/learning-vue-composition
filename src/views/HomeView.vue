e582f35efa871ccb99e1a645a5651696f2cf18b5
Erwin Macaraig <erwin.macaraig@enablesme.com.au>
2022-05-24 12:14:45 +0800

    implemented some options api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,40 @@
 
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div>
+    <button @click="decreaseConter" class="btn">-</button>
+    <span class="counter">{{counter}}</span>
+    <button @click="increaseCounter" class="btn">+</button>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      increaseCounter() {
+        this.counter++
+      },
+      decreaseConter(){
+        this.counter--
+      }
+    }
+  }
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
